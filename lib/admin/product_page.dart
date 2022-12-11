4f4d66cdab16e0208a9ffe22a84bf0b7eeb8b087
Dilshodjon <89077434+Yorvoration@users.noreply.github.com>
2022-12-11 20:41:42 +0500

    Flutter ombor projekt

--- lib/admin/product_page.dart
+++ lib/admin/product_page.dart
@@ -1,8 +1,13 @@
 
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class ProductPage extends StatefulWidget {
-  const ProductPage({super.key, required category_id, required category_name});
+  var category_id;
+  var category_name;
+  ProductPage({super.key, this.category_id, this.category_name});
 
   @override
   _ProductPageState createState() => _ProductPageState();
@@ -10,11 +15,26 @@ class ProductPage extends StatefulWidget {
 
 class _ProductPageState extends State<ProductPage> with SingleTickerProviderStateMixin {
 
-  @override
-  void initState() {
-    super.initState();
+
+ // https://golalang-online-sklad-production.up.railway.app/getProductsByCategory?categoryId=iYVt395gIGixhDlAmW9OYbCFcs0C2lBe
+  Future<void> _getProductsByCategory() async {
+    var catId = widget.category_id;
+    final response = await http.get(
+      Uri.parse('https://golalang-online-sklad-production.up.railway.app/getProductsByCategory?categoryId=$catId'),
+    );
+    if (response.statusCode == 200) {
+      final data = jsonDecode(response.body);
+      print(data);
+    } else {
+      throw Exception('Failed to load data');
+    }
   }
 
+  @override
+  void initState() {
+    _getProductsByCategory();
+    super.initState();
+  }
 
 
   @override
