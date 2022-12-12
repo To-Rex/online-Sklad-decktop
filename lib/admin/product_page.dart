2234073954b3391311ca3f8f7ad517905e4f6da5
Dilshodjon <89077434+Yorvoration@users.noreply.github.com>
2022-12-12 18:15:20 +0500

    Flutter ombor projekt

--- lib/admin/product_page.dart
+++ lib/admin/product_page.dart
@@ -29,6 +29,18 @@ class _ProductPageState extends State<ProductPage>
   var productNumber = [];
 
   Future<void> _getProductsByCategory() async {
+    productId.clear();
+    productName.clear();
+    productDescription.clear();
+    productPrice.clear();
+    productCategoryId.clear();
+    productBenefit.clear();
+    productStock.clear();
+    productStatus.clear();
+    productDate.clear();
+    productSellerId.clear();
+    productNumber.clear();
+
     var catId = widget.category_id;
     print(catId);
     final response = await http.get(
@@ -37,6 +49,7 @@ class _ProductPageState extends State<ProductPage>
     );
     if (response.statusCode == 200) {
       final data = jsonDecode(response.body);
+      print(data.length);
       for (var i = 0; i < data.length; i++) {
         setState(() {
           productId.add(data['data'][i]['product_id']);
@@ -147,13 +160,166 @@ class _ProductPageState extends State<ProductPage>
         body: Column(
           children: [
             Expanded(
-              child: ListView.builder(
-                itemCount: productName.length,
-                itemBuilder: (context, index) {
-                  return ListTile(
-                    title: Text(productName[index]),
-                  );
-                },
+              child: ListView(
+                children: [
+                  GestureDetector(
+                    onTap: () {
+                      //read product
+                    },
+                      child: Column(
+                        children: [
+                          Container(
+                            margin: const EdgeInsets.only(
+                                left: 10, right: 10, top: 10, bottom: 10),
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(10),
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.grey.withOpacity(0.35),
+                                  //color: Color.fromARGB(255, 221, 221, 221),
+                                  spreadRadius: 1,
+                                  blurRadius: 10,
+                                  offset: const Offset(0, 3),
+                                ),
+                              ],
+                            ),
+                            child: Column(
+                              children: [
+                                SizedBox(
+                                    height: MediaQuery.of(context).size.height / 50),
+                                //product qo`shish
+                                Row(
+                                  //mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    SizedBox(
+                                      width: MediaQuery.of(context).size.width / 50,
+                                    ),
+                                    const Text(
+                                      'Yangi mahsulot',
+                                      style: TextStyle(
+                                          fontSize: 20,
+                                          color: Colors.black,
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                    const Expanded(
+                                      child: SizedBox(),
+                                    ),
+                                    IconButton(onPressed:
+                                        () {},
+                                        icon: const Icon(Icons.add_circle_outline_outlined,color: Colors.deepPurpleAccent,size: 30,)),
+                                    SizedBox(
+                                      width: MediaQuery.of(context).size.width / 35,
+                                    ),
+                                  ],
+                                ),
+                                SizedBox(
+                                    height: MediaQuery.of(context).size.height / 50),
+                              ],
+                            ),
+                          ),
+                          for (var i = 0; i < productId.length; i++)
+                            Container(
+                              margin: const EdgeInsets.only(
+                                  left: 10, right: 10, top: 10, bottom: 10),
+                              decoration: BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: BorderRadius.circular(10),
+                                boxShadow: [
+                                  BoxShadow(
+                                    color: Colors.grey.withOpacity(0.35),
+                                    //color: Color.fromARGB(255, 221, 221, 221),
+                                    spreadRadius: 1,
+                                    blurRadius: 10,
+                                    offset: const Offset(0, 3),
+                                  ),
+                                ],
+                              ),
+                              child: Column(
+                                children: [
+                                  SizedBox(
+                                      height: MediaQuery.of(context).size.height / 50),
+                                  Row(
+                                    children: [
+                                      SizedBox(
+                                          width:
+                                          MediaQuery.of(context).size.width * 0.01),
+                                      Container(
+                                        margin: const EdgeInsets.only(left: 10),
+                                        child: SvgPicture.asset(
+                                          'assets/productIcon.svg',
+                                          height: 50,
+                                          width: 50,
+                                        ),
+                                      ),
+                                      SizedBox(
+                                          width:
+                                          MediaQuery.of(context).size.width * 0.01),
+                                      Expanded(
+                                        child: Column(
+                                          crossAxisAlignment: CrossAxisAlignment.start,
+                                          children: [
+                                            Text(
+                                              productName[i],
+                                              style: const TextStyle(
+                                                  fontSize: 18,
+                                                  fontWeight: FontWeight.bold),
+                                            ),
+                                            const SizedBox(
+                                              height: 5,
+                                            ),
+                                            Text(
+                                              '${productPrice[i]} so\'m',
+                                              style: const TextStyle(
+                                                  fontSize: 16,fontWeight: FontWeight.bold),
+                                            ),
+                                            const SizedBox(
+                                              height: 5,
+                                            ),
+                                            Text(
+                                              productDescription[i],
+                                              style: const TextStyle(
+                                                  fontSize: 14, color: Colors.grey,fontWeight: FontWeight.bold),
+                                            ),
+
+                                          ],
+                                        ),
+                                      ),
+                                      IconButton(
+                                        onPressed: () {},
+                                        icon: SvgPicture.asset(
+                                          'assets/editIcon.svg',
+                                          height: 25,
+                                          width: 25,
+                                          color: Colors.deepPurpleAccent,
+                                        ),
+                                      ),
+                                      SizedBox(
+                                        width: MediaQuery.of(context).size.width * 0.005,
+                                      ),
+                                      IconButton(
+                                        onPressed: () {},
+                                        icon: SvgPicture.asset(
+                                          'assets/deleteIcon.svg',
+                                          height: 25,
+                                          width: 25,
+                                          color: Colors.deepPurpleAccent,
+                                        ),
+                                      ),
+                                      SizedBox(
+                                        width: MediaQuery.of(context).size.width / 40,
+                                      ),
+                                    ],
+                                  ),
+                                  SizedBox(
+                                      height: MediaQuery.of(context).size.height / 50),
+                                ],
+                              ),
+                            ),
+                        ],
+                      ),
+                  ),
+                ],
               ),
             ),
           ],
