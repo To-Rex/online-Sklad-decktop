1b439f8a2d95b2d6a94e53dad2e826cd80dc58b3
Dilshodjon <89077434+Yorvoration@users.noreply.github.com>
2022-12-16 02:57:41 +0500

    Flutter ombor projekt

---
+++ lib/admin/user_page.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class UserPage extends StatefulWidget {
+  const UserPage({super.key});
+
+  @override
+  _UserPageState createState() => _UserPageState();
+}
+
+class _UserPageState extends State<UserPage>  with SingleTickerProviderStateMixin {
+
+  late TabController _tabController;
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 3, vsync: this);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(0), // here the desired height
+        child: AppBar(
+          backgroundColor: const Color.fromRGBO(33, 158, 188, 10),
+        ),
+      ),
+      body: Column(
+        children: const [
+          Text(''),
+          //floating button default
+        ],
+      ),
+    );
+  }
+}
