6cbbbd9ba6f63951783903269ca2cc5b3d6f2ae8
Dilshodjon <89077434+Yorvoration@users.noreply.github.com>
2022-12-18 11:57:16 +0500

    Flutter ombor projekt

--- lib/admin/tarnsaktion_page.dart
+++ lib/admin/tarnsaktion_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class TransaktionsPage extends StatefulWidget {
   const TransaktionsPage({super.key});
@@ -25,6 +26,75 @@ class _TransktionPageState extends State<TransaktionsPage>  with SingleTickerPro
           elevation: 0.4,
           actionsIconTheme: const IconThemeData(color: Colors.black),
           iconTheme: const IconThemeData(color: Colors.black),
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const Expanded(
+                child: SizedBox(),
+              ),
+              //iconbutton
+              SizedBox(
+                height: 30,
+                width: MediaQuery.of(context).size.width / 4,
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: const Color.fromARGB(255, 221, 221, 221),
+                    border: Border.all(
+                        color: const Color.fromARGB(255, 221, 221, 221),
+                        width: 1),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: TextField(
+                    cursorColor: Colors.deepPurpleAccent,
+                    textAlign: TextAlign.justify,
+                    textInputAction: TextInputAction.next,
+                    onChanged: (value) {
+
+                    },
+                    decoration: const InputDecoration(
+                      contentPadding: EdgeInsets.only(left: 10, right: 10),
+                      border: InputBorder.none,
+                      hintText: 'Qidirish',
+                      suffixIcon: Icon(
+                        Icons.search,
+                        color: Colors.deepPurpleAccent,
+                        size: 20,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                width: MediaQuery.of(context).size.width / 50,
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.05,
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: const Color.fromARGB(255, 221, 221, 221),
+                    border: Border.all(
+                        color: const Color.fromARGB(255, 221, 221, 221),
+                        width: 1),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: IconButton(
+                    hoverColor: Colors.transparent,
+                    splashColor: Colors.transparent,
+                    highlightColor: Colors.white,
+                    color: Colors.white,
+                    onPressed: () {
+                    },
+                    icon: SvgPicture.asset(
+                      'assets/sort.svg',
+                      color: Colors.deepPurpleAccent,
+                      height: 60,
+                      width: 60,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
       body: Column(
