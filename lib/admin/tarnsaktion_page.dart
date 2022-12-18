38f1e72863d23dd4e3c9b6669263312a632f8143
Dilshodjon <89077434+Yorvoration@users.noreply.github.com>
2022-12-18 11:23:17 +0500

    Flutter ombor projekt

---
+++ lib/admin/tarnsaktion_page.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class TransaktionsPage extends StatefulWidget {
+  const TransaktionsPage({super.key});
+
+  @override
+  _TransktionPageState createState() => _TransktionPageState();
+}
+
+class _TransktionPageState extends State<TransaktionsPage>  with SingleTickerProviderStateMixin {
+
+  late TabController _tabController;
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 3, vsync: this);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(0), // here the desired height
+        child: AppBar(
+          backgroundColor: const Color.fromRGBO(33, 158, 188, 10),
+        ),
+      ),
+      body: Column(
+        children: const [
+          Text(''),
+          //floating button default
+        ],
+      ),
+    );
+  }
+}
